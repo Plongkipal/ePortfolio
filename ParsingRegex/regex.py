4cf92c3ae3cc1fe452f745601848a64dd1ebb59e
Emmanskies <137289387+Plongkipal@users.noreply.github.com>
2024-12-28 23:40:33 +0800

    Delete ParsingRegex directory

--- ParsingRegex/regex.py
+++
@@ -1,6 +0,0 @@
-from parser import insert_explicit_concat_operator, to_postfix
-from nfa import to_nfa, recognize
-
-def create_matcher(exp):
-    nfa = to_nfa(exp)  # Using the tree-based NFA construction
-    return lambda word: recognize(nfa, word)
